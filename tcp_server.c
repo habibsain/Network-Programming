cf45c47f85dec260af96ecec63d29dee5295e381
habibsain <habibsain25@gmail.com>
2025-07-07 22:12:24 +0530

    Initialise the socket and socket addr

---
+++ tcp_server.c
@@ -0,0 +1,27 @@
+//tcp_server.c
+
+#include "include/socket_header.h"
+
+int main()
+{
+    #if defined(_WIN32)
+    WSDATA d;
+    if(WSAStartup(MAKEWORD(2, 2), &d))
+    {
+        fprintf(stderr, "Failed to initialize socket");
+        returrn -1;
+    }
+    #endif
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    
+    struct addrinfo *host_addr;
+    getaddrinfo(0, "8080", &hints, &host_addr);
+
+
+    SOCKET host_socket;
+
+    return 0;
+}
