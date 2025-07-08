037631a57ef391790fc4f0e9567f3ba9d84946ba
habibsain <habibsain25@gmail.com>
2025-07-08 14:02:03 +0530

    added socket creation, binding and  listening

--- tcp_server.c
+++ tcp_server.c
@@ -20,10 +20,36 @@ int main()
     struct addrinfo *host_addr;
     getaddrinfo(0, "8080", &hints, &host_addr);
 
-    //bind
-
-
+    //Create socket
     SOCKET host_socket;
+    host_socket = create_socket(host_addr);
+    if (!ISVALIDSOCKET(host_socket))
+    {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRORNO());
+        return 1;
+    }
+
+
+    //binding host(listening) socket to host(local) address
+    printf("Binding-----\n");
+    
+    if (bind(host_socket, host_addr->ai_addr, host_addr->ai_addrlen))
+    {
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRORNO());
+        return 1;
+    }
+
+    //Listen on the socket in host server
+    printf("Listening-----\n");
+    int max_num = 20;
+    if (listen(host_socket, max_num) < 0)
+    {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRORNO());
+        return 1;
+    }
+
+    
+
 
     return 0;
 }
