81e626ef145485664deebe23900f04d90d55e067
habibsain <habibsain25@gmail.com>
2025-07-14 16:08:35 +0530

    edited struct

--- udp/udp_client.c
+++ udp/udp_client.c
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
 
     if (!ISVALIDSOCKET(socket_peer))
     {
-        printf(stderr, "socket() failed. (%d)\n", GETSOCKETERRORNO());
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRORNO());
         return 1;
     }
 
